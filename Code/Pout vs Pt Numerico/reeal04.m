7cedd50e4f47ffcf345e175ced2609d91ec90c69
Vignonfidele <110578891+Vignonfidele@users.noreply.github.com>
2022-10-06 11:04:08 -0300

    Delete Code directory

--- Code/Pout vs Pt Numerico/reeal04.m
+++
@@ -1,9 +0,0 @@
-clc
-clear all 
-close all 
-alpha=4;
-N=2e6;
-d1=0.4; d2=0.6;                                                               
-g1=(d1^(-alpha));   g2=(d2^(-alpha));
-realizacaoAr=sqrt(g1)*randraw('nakagami', 1, [1 N]);  
-realizacaoBr=sqrt(g2)*randraw('nakagami', 1, [1 N]);
--- Code/Pout vs d/Untitled3.m
+++
@@ -1,9 +0,0 @@
-clc
-clear all 
-close 
-R = rand(4);
- h = plot(R)
- legend(h, '1', '2', '3', '4')
- hold on
- h2 = plot(mean(R), 'k', 'LineWidth', 2)
- legend([h; h2], '1', '2', '3', '4', 'Total')
